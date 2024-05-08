1ee5c0dee79aaa2bcdd7fb88494a0ac77394a23f
Josemaria <153980980+Wojtya@users.noreply.github.com>
2024-05-08 21:14:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 // Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+//document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    //anchor.addEventListener('click', function (e) {
+        //e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+        //document.querySelector(this.getAttribute('href')).scrollIntoView({
+      //      behavior: 'smooth'
+    //    });
+  //  });
+//});
 
 // Back to top button
-window.onscroll = function () {
+//window.onscroll = function () {
     scrollFunction();
-};
+//};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("scrollTopBtn").style.display = "block";
-    } else {
-        document.getElementById("scrollTopBtn").style.display = "none";
-    }
-}
+//function scrollFunction() {
+    //if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      //  document.getElementById("scrollTopBtn").style.display = "block";
+    //} else {
+        //document.getElementById("scrollTopBtn").style.display = "none";
+  //  }
+//}
 
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+//function topFunction() {
+    //document.body.scrollTop = 0;
+  //  document.documentElement.scrollTop = 0;
+//}
