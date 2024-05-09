4587ab6431738b33a956e3fcb73862ba64756915
Josemaria <153980980+Wojtya@users.noreply.github.com>
2024-05-09 19:13:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
-// Smooth scrolling
-//document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    //anchor.addEventListener('click', function (e) {
-        //e.preventDefault();
+const navLinks = document.querySelectorAll('nav a');
 
-        //document.querySelector(this.getAttribute('href')).scrollIntoView({
-      //      behavior: 'smooth'
-    //    });
-  //  });
-//});
-
-// Back to top button
-//window.onscroll = function () {
-    scrollFunction();
-//};
-
-//function scrollFunction() {
-    //if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      //  document.getElementById("scrollTopBtn").style.display = "block";
-    //} else {
-        //document.getElementById("scrollTopBtn").style.display = "none";
-  //  }
-//}
-
-//function topFunction() {
-    //document.body.scrollTop = 0;
-  //  document.documentElement.scrollTop = 0;
-//}
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetSection = document.getElementById(this.getAttribute('href').slice(1));
+        targetSection.scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
